8613983cdc02c835decc69cb753d05dcbdf4ff11
SDTakeuchi <kuranku191952996@gmail.com>
2020-09-18 21:00:34 +0900

    fix

--- my-laravel-app/resources/views/home.blade.php
+++ my-laravel-app/resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="home-content">
         <ul>
-            <li><a href="{{ route('register_book') }}">本を登録</a></li>
+            <li><a href="{{ route('register_book') }}">登録！！！！</a></li>
             <li><a href="{{ route('show_books') }}">登録一覧</a></li>
         </ul>
     </div>
